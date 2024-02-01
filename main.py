fe5b4b7f1704764da6a06529deb236ff8274730e
MClose23 <123100717+MClose23@users.noreply.github.com>
2024-02-01 15:46:40 -0600

    1.0 rough draft

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #REMOVE PASS AND FIX THE FUNCTION
 def sum_of_products(list1, list2):
-    pass
+    list1 = int(input())
+    list2 = int(input())
+    return
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
+    print((zip.list1 + zip.list2) , + (zip.list1[1] + zip.list2[1]) + (zip.list1[2] * zip.list2[2]))
+else:
     pass
