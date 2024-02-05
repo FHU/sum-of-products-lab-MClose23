921d17ce643f6fec967c82147adfdf7bbeb23f4c
MClose23 <123100717+MClose23@users.noreply.github.com>
2024-02-05 12:09:25 -0600

    commit 2.0

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def sum_of_products(list1, list2):
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
-    print((zip.list1 + zip.list2) , + (zip.list1[1] + zip.list2[1]) + (zip.list1[2] * zip.list2[2]))
+    
+elif len(list1) == len(list2):
+ 
+    output = sum(int(list1_num) * int(list2_num) for list1_num, list2_num in zip(list1, list2))
+    print(output)
+
 else:
-    pass
+    print("error.")
