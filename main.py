ed9c12d526f07be0c6d7c3208411aaefdec6f57e
MClose23 <123100717+MClose23@users.noreply.github.com>
2024-02-06 15:29:54 -0600

    revised 6.1

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #REMOVE PASS AND FIX THE FUNCTION
 def sum_of_products(list1, list2):
-    list1 = int(input())
-    list2 = int(input())
-    return
+    
+    output = sum(int(list1_num) * int(list2_num) for list1_num, list2_num in zip(list1, list2))
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
     len(list1) == len(list2)
- 
-    output = sum(int(list1_num) * int(list2_num) for list1_num, list2_num in zip(list1, list2))
+    
+    list1 = input()
+    list2 = input()
+
+                               
     print(output)
 
 else:
