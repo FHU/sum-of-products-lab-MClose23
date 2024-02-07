a2efd92069467ea5c9a2521e6a88bf89ab3fe0b0
MClose23 <123100717+MClose23@users.noreply.github.com>
2024-02-06 20:04:44 -0600

    testing 3.0

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 #REMOVE PASS AND FIX THE FUNCTION
 def sum_of_products(list1, list2):
-    
+    if len(list1) == len(list2):
     output = sum(int(list1_num) * int(list2_num) for list1_num, list2_num in zip(list1, list2))
+        return output
+    else:
+        return "error"
+
+
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
-    len(list1) == len(list2)
+
     
     list1 = input()
     list2 = input()
 
-                               
-    print(output)
-
-else:
-    print("error.")
+final_output = sum_of_products(list1, list2)
+print(final_output)
